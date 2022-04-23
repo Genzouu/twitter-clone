f07d638a0aa66a3cd198e0f514cae64b193ce686
Genzou <spookyjoness@outlook.com>
2022-04-23 13:47:20 +0800

    added some placeholder data

--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -15,25 +15,29 @@ export default function Profile() {
             <BsArrowLeftShort className="return-arrow-icon" />
             <div className="username-tweet-count-container">
                <p className="header-username">Username</p>
-               <p className="tweet-count">11 Tweets</p>
+               <p className="tweet-count">10 Tweets</p>
             </div>
          </div>
          <div className="top-section">
             <div className="banner"></div>
-            <img src="" alt="" className="profile-picture" />
-            <button className="edit-profile"></button>
-            <p className="username"></p>
-            <p className="handle"></p>
-            <p className="bio"></p>
-            <p className="birthday"></p>
-            <p className="account-created-date"></p>
-            <p className="following"></p>
-            <p className="followers"></p>
+            {/* <img
+               src="http://getdrawings.com/free-icon-bw/anonymous-avatar-icon-19.png"
+               alt=""
+               className="profile-picture"
+            /> */}
+            <button className="edit-profile">Edit Profile</button>
+            <p className="username">Username</p>
+            <p className="handle">@username</p>
+            <p className="bio">This is a bio.</p>
+            <p className="birthday">Birthday: 1/1/2000</p>
+            <p className="account-created-date">Using Twitter from 1/2000</p>
+            <p className="following">10 Following</p>
+            <p className="followers">10 Followers</p>
             <div className="sections">
-               <button className="tweets"></button>
-               <button className="tweets-replies"></button>
-               <button className="media"></button>
-               <button className="likes"></button>
+               <button className="tweets">Tweets</button>
+               <button className="tweets-replies">Tweets and Replies</button>
+               <button className="media">Media</button>
+               <button className="likes">Likes</button>
             </div>
          </div>
          <div className="tweet-list"></div>
--- src/Components/RPTopic.tsx
+++ src/Components/RPTopic.tsx
@@ -1,3 +1,11 @@
 export default function RPTopic() {
-   return <div className="right-panel-topic"></div>;
+   return (
+      <div className="right-panel-topic">
+         <p className="category"></p>
+         <p className="time"></p>
+         <p className="title"></p>
+         <img src="" alt="" className="picture" />
+         {/* 3 dots if there isn't a picture */}
+      </div>
+   );
 }
